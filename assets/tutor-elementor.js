22ff826972b36c1dd8a51724575ca53ba73dc680
sksaju <sksaaju@gmail.com>
2020-11-20 02:47:03 +0600

    update assets and assetsManager

--- assets/tutor-elementor.js
+++ assets/tutor-elementor.js
@@ -1,6 +1,63 @@
 'use strict';
 (function ($) {
+	/**
+	 * Course Enroll Button
+	 * @since 1.0.0
+	 */
+	function enroll_button($scope) {
+		/**
+		 * if default button 
+		 * add button class
+		 */
+		var button_type = $scope.find("#etlms_enroll_btn_type").val();
+		var cart_icon = $scope.find("#etlms_enroll_btn_cart").val();
+
+		/**
+		 * find tutor-loop-cart-btn-wrap from etlms-carousel-footer
+		 * remove class for setting new style
+		 */
+		var carousel_footer = $scope.find(".etlms-carousel-footer").find('.tutor-loop-cart-btn-wrap');
+
+		if (carousel_footer) {
+			carousel_footer.removeClass('tutor-loop-cart-btn-wrap');
+			carousel_footer.addClass('etlms-loop-cart-btn-wrap');
+			/**
+			 * add to cart has default button class
+			 * remove it
+			 * make it dynamic from elementor
+			 */
+			if (carousel_footer.children("a").hasClass('button')) {
+				carousel_footer.children("a").removeClass('button');
+			};
+		}
+
+		/**
+		 * if default button 
+		 * add button class
+		 */
+		if (button_type == 'default' || button_type == 'default_with_cart_icon') {
+			carousel_footer.children("a").addClass('tutor-button');
+		}
+
+		/**
+		 * if cart icon set from elementor
+		 * add cart icon to all button
+		 */
+		if (button_type == 'text_with_cart' || button_type == 'default_with_cart_icon') {
+			var length = carousel_footer.length;
+			var i = 0;
+			for (i; i < length; i++) {
+				var text = carousel_footer.children("a")[i].innerHTML;
+				carousel_footer.children("a")[i].innerHTML = `<i class="${cart_icon}" aria-hidden="true"></i> ${text}`;
+			}
+		}
+	}
+
 	jQuery(window).on('elementor/frontend/init', function () {
+		/**
+		 * Course Curriculum
+		 * @since 1.0.0
+		 */
 		elementorFrontend.hooks.addAction('frontend/element_ready/etlms-course-curriculum.default', function ($scope, $) {
 			var active_icon = $scope.find("#etlms-course-topic-active").val();
             var inactive_icon = $scope.find("#etlms-course-topic-inactive").val();
@@ -12,6 +69,91 @@
 					height: 'toggle'
 				}, 'slow');
 			});
+		});
+
+		/**
+		 * Course Carousel
+		 * @since 1.0.0
+		 */
+		elementorFrontend.hooks.addAction('frontend/element_ready/etlms-course-carousel.default', function ($scope, $) {
+			/**
+			 * get all carousel settings
+			 */
+			var desktop = $scope.find("#etlms_carousel_settings").attr('desktop');
+			var medium = $scope.find("#etlms_carousel_settings").attr('medium');
+			var mobile = $scope.find("#etlms_carousel_settings").attr('mobile');
+			var carousel_arrows = $scope.find("#etlms_carousel_settings").attr('arrows');
+			var carousel_dots = $scope.find("#etlms_carousel_settings").attr('dots');
+			var carousel_transition = $scope.find("#etlms_carousel_settings").attr('transition');
+			var carousel_center = $scope.find("#etlms_carousel_settings").attr('center');
+			var smooth_scroll = $scope.find("#etlms_carousel_settings").attr('smoth_scroll');
+			var carousel_autoplay = $scope.find("#etlms_carousel_settings").attr('auto_play');
+			var carousel_auto_play_speed = $scope.find("#etlms_carousel_settings").attr('auto_play_speed');
+			var carousel_infinite_loop = $scope.find("#etlms_carousel_settings").attr('infinite_loop');
+			var carousel_pause_on_hover = $scope.find("#etlms_carousel_settings").attr('pause_on_hover');
+			var carousel_pause_on_interaction = $scope.find("#etlms_carousel_settings").attr('pause_on_interaction');
+
+			carousel_arrows == 'yes' ? carousel_arrows = true : carousel_arrows = false;
+			carousel_dots == 'yes' ? carousel_dots = true : carousel_dots = false;
+			carousel_transition = Number(carousel_transition);
+			carousel_center == 'yes' ? carousel_center = true : carousel_center = false;
+			carousel_autoplay == 'yes' ? carousel_autoplay = true : carousel_autoplay = false;
+			Number(carousel_auto_play_speed);
+			carousel_infinite_loop == 'yes' ? carousel_infinite_loop = true : carousel_infinite_loop = false;
+			carousel_pause_on_hover == 'yes' ? carousel_pause_on_hover = true : carousel_pause_on_hover = false;
+			carousel_pause_on_interaction == 'yes' ? carousel_pause_on_interaction = true : carousel_pause_on_interaction = false;
+
+			/**
+			 * applying all settings here
+			 */
+			$scope.find('#etlms-slick-responsive').slick({
+				dots: carousel_dots,
+				arrows: carousel_arrows,
+				infinite: false,
+				speed: carousel_transition,
+				centerMode: carousel_center,
+				autoplay: carousel_autoplay,
+				autoplaySpeed: carousel_auto_play_speed,
+				infinite: carousel_infinite_loop,
+				pauseOnHover: carousel_pause_on_hover,
+				// ineraction
+				pauseOnFocus: carousel_pause_on_interaction,
+				cssEase: smooth_scroll,
+				slidesToShow: desktop,
+				slidesPerRow: desktop,
+				slidesToScroll: 1,
+				prevArrow: $scope.find('.etlms-carousel-arrow-prev'),
+				nextArrow: $scope.find('.etlms-carousel-arrow-next'),
+
+				responsive: [
+					{
+						breakpoint: 1024,
+						settings: {
+							slidesToShow: medium,
+							slidesToScroll: 1,
+							infinite: true,
+							dots: true
+						}
+					},
+					{
+						breakpoint: 576,
+						settings: {
+							slidesToShow: mobile,
+							slidesToScroll: 1
+						}
+					}
+				]
+			});
+
+			enroll_button($scope, $);  
+		});
+
+		/**
+		 * Course List
+		 * @since 1.0.0
+		 */
+		elementorFrontend.hooks.addAction('frontend/element_ready/etlms-course-list.default', function ($scope, $) {
+			enroll_button($scope, $);
 		});
 	});
 })(jQuery);
--- classes/AssetsManager.php
+++ classes/AssetsManager.php
@@ -21,42 +21,12 @@ class AssetsManager {
      * @since 1.0.0
      */
     public static function init() {
-        /* Additional css */
-        add_action('wp_enqueue_scripts', [__CLASS__, 'enqueue_additional_scripts']);
         /* Editor Scripts */
         add_action('elementor/editor/before_enqueue_scripts', [__CLASS__, 'enqueue_editor_scripts']);
 
-
-         // Enqueue Scripts
-
-        add_action( 'elementor/frontend/after_register_scripts', [ __CLASS__, 'etlms_elementor_scripts' ] );
-
-        //Enqueue Styles
-        add_action( 'elementor/frontend/after_enqueue_styles', [ __CLASS__, 'etlms_elementor_styles' ] );
-
-
-    }
-
-    /**
-     * Enqueue additional scripts
-     * @since 1.0.0
-     */
-    public static function enqueue_additional_scripts() {
-        wp_enqueue_style(
-            'tutor-elementor',
-            ETLMS_ASSETS . 'tutor-elementor.css',
-            null,
-            ETLMS_VERSION
-        );        
-
-        wp_enqueue_style(
-            'tutor-shewa-elementor',
-            ETLMS_ASSETS . 'tutor-shewa-elementor.css',
-            null,
-            //ETLMS_VERSION
-            filemtime(ETLMS_DIR_PATH.'/assets/tutor-shewa-elementor.css')
-        );       
-
+        /* Enqueue elementor styles and scripts */
+        add_action('wp_enqueue_scripts', [__CLASS__, 'enqueue_elementor_styles']);
+        add_action('wp_enqueue_scripts', [__CLASS__, 'enqueue_elementor_scripts']);
     }
 
     /**
@@ -72,8 +42,11 @@ class AssetsManager {
         );    
     }
 
-    public static function etlms_elementor_styles(){
-        //Register FontAwesome for fallback
+    /**
+     * Enqueue styles
+     * @since 1.0.0
+     */
+    public static function enqueue_elementor_styles() {
         wp_register_style(
             'font-awesome-5-all',
             ELEMENTOR_ASSETS_URL . 'lib/font-awesome/css/all.min.css',
@@ -100,42 +73,35 @@ class AssetsManager {
             'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css',
             null,
             ETLMS_VERSION
-        );  
-    } 
-    public static function etlms_elementor_scripts(){
+        );
 
-        wp_register_script( 'etlms-slick-library', 
-            'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', 
-            array(  ), 
-            '1.0.0', true ); 
+        wp_enqueue_style(
+            'tutor-elementor-css',
+            ETLMS_ASSETS . 'tutor-elementor.css',
+            null,
+            ETLMS_VERSION
+        );
+    }
 
-        wp_register_script (
-            'etlms-slick-slider',
-            ETLMS_ASSETS.'slick-slider.js',
-            array('etlms-slick-library'),
-            filemtime(ETLMS_DIR_PATH.'/assets/slick-slider.js'),
-            true
-        );          
-
-        wp_register_script (
-            'etlms-enroll-button',
-            ETLMS_ASSETS.'enroll-button.js',
-            array('jquery'),
-            filemtime(ETLMS_DIR_PATH.'/assets/enroll-button.js'),
-            true
-        );        
-
-        wp_register_script (
-            'tutor-elementor-js',
-            ETLMS_ASSETS.'tutor-elementor.js',
-            array('jquery'),
-            filemtime(ETLMS_DIR_PATH.'/assets/tutor-elementor.js'),
+    /**
+     * Enqueue scripts
+     * @since 1.0.0
+     */
+    public static function enqueue_elementor_scripts() {
+        wp_enqueue_script(
+            'etlms-slick-library',
+            'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js',
+            array(),
+            '1.0.0',
             true
         );
-        wp_enqueue_script('tutor-elementor-js');
 
-        wp_enqueue_script( 'etlms-slick-library');
-        wp_enqueue_script( 'etlms-slick-slider');
-        wp_enqueue_script('etlms-enroll-button');
+        wp_enqueue_script(
+            'tutor-elementor-js',
+            ETLMS_ASSETS . 'tutor-elementor.js',
+            array('jquery'),
+            ETLMS_VERSION,
+            true
+        );
     }
 }
