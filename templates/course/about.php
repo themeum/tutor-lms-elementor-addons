f3ec8fc6944a6031e6325d2741e83f550ca22869
sksaju <sksaaju@gmail.com>
2021-03-16 16:36:01 +0600

    update course about template

--- templates/course/about.php
+++ templates/course/about.php
@@ -4,7 +4,7 @@ $excerpt = tutor_get_the_excerpt();
 <div class="etlms-course-summery">
     <h4 class="tutor-segment-title"><?php esc_html_e($settings['section_title_text'], 'tutor-lms-elementor-addons'); ?></h4>
     <div class="tutor-course-excerpt">
-        <?php echo $excerpt;
+        <?php
         if (!empty($excerpt)) {
             echo $excerpt;
         } else if (\Elementor\Plugin::instance()->editor->is_edit_mode()) {
