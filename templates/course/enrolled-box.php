f98e9690112bd35b6de804b063d2d0fd833afdf6
shewa12 <shewa12kpi@gmail.com>
2021-11-25 14:50:39 +0600

    course topic and enrollment addons updated

--- templates/course/enrolled-box.php
+++ templates/course/enrolled-box.php
@@ -22,7 +22,7 @@ global $wp_query;
         <?php }
         }
         ?>
-        <?php tutor_course_mark_complete_html(); ?>
+        <?php //tutor_course_mark_complete_html(); ?>
 
         <?php do_action('tutor_course/single/actions_btn_group/after'); ?>
     </div>
