0dccd380b6d1c10d9aab8bc7f1da77ce20e555c1
shewa12 <shewa12kpi@gmail.com>
2021-11-29 13:15:19 +0600

    course topics template updated with v2 & CourseRating class formated

--- includes/addons/CourseRating.php
+++ includes/addons/CourseRating.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Course Ratting Addon
+ *
  * @since 1.0.0
  */
 
@@ -9,140 +10,142 @@ namespace TutorLMS\Elementor\Addons;
 use Elementor\Controls_Manager;
 use Elementor\Group_Control_Typography;
 
-if (!defined('ABSPATH')) exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
 
 class CourseRating extends BaseAddon {
 
-    use \TutorLMS\Elementor\AddonsTrait;
+	use \TutorLMS\Elementor\AddonsTrait;
 
-    private static $prefix_class_layout = "elementor-layout-";
-    private static $prefix_class_alignment = "elementor-align-"; 
+	private static $prefix_class_layout    = 'elementor-layout-';
+	private static $prefix_class_alignment = 'elementor-align-';
 
-    public function get_title() {
-        return __('Course Rating', 'tutor-lms-elementor-addons');
-    }
-
-    protected function register_content_controls() {
-		$this->start_controls_section(
-            'course_title_content',
-            [
-                'label' => __('General Settings', 'tutor-lms-elementor-addons'),
-            ]
-        );
-        $this->add_responsive_control(
-            'course_rating_align',
-            $this->etlms_alignment() //alignment
-        );
-
-        $this->end_controls_section();
+	public function get_title() {
+		return __( 'Course Rating', 'tutor-lms-elementor-addons' );
 	}
-    
-    protected function register_style_controls() {
-        $selector = '{{WRAPPER}} .tutor-single-course-rating';
-        $rating_group = $selector.' .tutor-star-rating-group';
-        $rating_count = $selector.' .tutor-single-rating-count';
 
-        //Style
-        $this->start_controls_section(
-            'course_style_section',
-            array(
-                'label' => __('Rating Stars', 'tutor-lms-elementor-addons'),
-                'tab' => Controls_Manager::TAB_STYLE,
-            )
-        );
+	protected function register_content_controls() {
+		$this->start_controls_section(
+			'course_title_content',
+			array(
+				'label' => __( 'General Settings', 'tutor-lms-elementor-addons' ),
+			)
+		);
+		$this->add_responsive_control(
+			'course_rating_align',
+			$this->etlms_alignment() // alignment
+		);
 
-        $this->add_control(
-            'course_rating_star_color',
-            [
-                'label'     => __('Star Color', 'tutor-lms-elementor-addons'),
-                'type'      => Controls_Manager::COLOR,
-                'selectors' => [
-                    $rating_group => 'color: {{VALUE}};',
-                ],
-                'default'   => '#ED9700'
-            ]
-        );
+		$this->end_controls_section();
+	}
 
-        $this->add_responsive_control(
-            'course_rating_star_size',
-            [
-                'label' => __( 'Star Size', 'tutor-lms-elementor-addons' ),
-                'type' => Controls_Manager::SLIDER,
-                'size_units' => [ 'px' ],
-                'range' => [
-                    'px' => [
-                        'min' => 10,
-                        'max' => 100,
-                    ],
-                ],
-                'selectors' => [
-                    $rating_group.' i' => 'font-size: {{SIZE}}{{UNIT}};',
-                ],
-                'default' => [
+	protected function register_style_controls() {
+		$selector     = '{{WRAPPER}} .tutor-single-course-rating';
+		$rating_group = $selector . ' .tutor-star-rating-group';
+		$rating_count = $selector . ' .tutor-single-rating-count';
+
+		// Style
+		$this->start_controls_section(
+			'course_style_section',
+			array(
+				'label' => __( 'Rating Stars', 'tutor-lms-elementor-addons' ),
+				'tab'   => Controls_Manager::TAB_STYLE,
+			)
+		);
+
+		$this->add_control(
+			'course_rating_star_color',
+			array(
+				'label'     => __( 'Star Color', 'tutor-lms-elementor-addons' ),
+				'type'      => Controls_Manager::COLOR,
+				'selectors' => array(
+					$rating_group => 'color: {{VALUE}};',
+				),
+				'default'   => '#ED9700',
+			)
+		);
+
+		$this->add_responsive_control(
+			'course_rating_star_size',
+			array(
+				'label'      => __( 'Star Size', 'tutor-lms-elementor-addons' ),
+				'type'       => Controls_Manager::SLIDER,
+				'size_units' => array( 'px' ),
+				'range'      => array(
+					'px' => array(
+						'min' => 10,
+						'max' => 100,
+					),
+				),
+				'selectors'  => array(
+					$rating_group . ' i' => 'font-size: {{SIZE}}{{UNIT}};',
+				),
+				'default'    => array(
 					'size' => 16,
-                ],
-            ]
-        );
+				),
+			)
+		);
 
-        $this->add_group_control(
-            Group_Control_Typography::get_type(),
-            array(
-                'name'      => 'course_rating_typography',
-                'label'     => __('Typography', 'tutor-lms-elementor-addons'),
-                'selector'  => $rating_count,
-            )
-        );
+		$this->add_group_control(
+			Group_Control_Typography::get_type(),
+			array(
+				'name'     => 'course_rating_typography',
+				'label'    => __( 'Typography', 'tutor-lms-elementor-addons' ),
+				'selector' => $rating_count,
+			)
+		);
 
-        $this->add_control(
-            'course_rating_text_color',
-            [
-                'label'     => __('Text Color', 'tutor-lms-elementor-addons'),
-                'type'      => Controls_Manager::COLOR,
-                'selectors' => [
-                    $rating_count => 'color: {{VALUE}};',
-                ],
-                'default'   => '#525252'
-            ]
-        );
+		$this->add_control(
+			'course_rating_text_color',
+			array(
+				'label'     => __( 'Text Color', 'tutor-lms-elementor-addons' ),
+				'type'      => Controls_Manager::COLOR,
+				'selectors' => array(
+					$rating_count => 'color: {{VALUE}};',
+				),
+				'default'   => '#525252',
+			)
+		);
 
-        $this->add_responsive_control(
-            'gap',
-            [
-                'label' => __( 'Gap', 'tutor-lms-elementor-addons' ),
-                'type' => Controls_Manager::SLIDER,
-                'size_units' => [ 'px' ],
-                'range' => [
-                    'px' => [
-                        'min' => 0,
-                        'max' => 50,
-                    ],
-                ],
-                'selectors' => [
-                    $rating_group.' i' => 'margin-right: {{SIZE}}{{UNIT}};',
-                ],
-                'default' => [
+		$this->add_responsive_control(
+			'gap',
+			array(
+				'label'      => __( 'Gap', 'tutor-lms-elementor-addons' ),
+				'type'       => Controls_Manager::SLIDER,
+				'size_units' => array( 'px' ),
+				'range'      => array(
+					'px' => array(
+						'min' => 0,
+						'max' => 50,
+					),
+				),
+				'selectors'  => array(
+					$rating_group . ' i' => 'margin-right: {{SIZE}}{{UNIT}};',
+				),
+				'default'    => array(
 					'size' => 5,
-                ],
-            ]
-        );
+				),
+			)
+		);
 
-        $this->end_controls_section();
-    }
+		$this->end_controls_section();
+	}
 
-    protected function render($instance = []) {
-        $disable = (bool) get_tutor_option('disable_course_review');
-        if ($disable) {
-            if (\Elementor\Plugin::instance()->editor->is_edit_mode()) {
-                echo __('Please enable course review from tutor settings', 'tutor-lms-elementor-addons');
-            }
-            return;
-        }
+	protected function render( $instance = array() ) {
+		$disable = (bool) get_tutor_option( 'disable_course_review' );
+		if ( $disable ) {
+			if ( \Elementor\Plugin::instance()->editor->is_edit_mode() ) {
+				echo __( 'Please enable course review from tutor settings', 'tutor-lms-elementor-addons' );
+			}
+			return;
+		}
 
-        $course = etlms_get_course();
-        if ($course) {
-            ob_start();
-            include etlms_get_template('course/rating');
-            echo ob_get_clean();
-        }
-    }
+		$course = etlms_get_course();
+		if ( $course ) {
+			ob_start();
+			include etlms_get_template( 'course/rating' );
+			echo ob_get_clean();
+		}
+	}
 }
--- templates/course/course-topics.php
+++ templates/course/course-topics.php
@@ -1,20 +1,31 @@
-
-<div class="tutor-wrap">
-
 <?php
+/**
+ * Tabs menu items
+ *
+ * @package Course Topics
+ */
+
 	$course_nav_items = tutor_utils()->course_nav_items();
-?>
-	<div>
-		<?php// do_action( 'tutor_course/single/before/inner-wrap' ); ?>
+	unset( $course_nav_items['info'] );
+	add_filter(
+		'tutor_default_topics_active_tab',
+		function() {
+			return 'curriculum';
+		}
+	);
+	?>
+<div class="tutor-wrap">
+		<?php do_action( 'tutor_course/single/before/inner-wrap' ); ?>
 		<div class="tutor-default-tab tutor-course-details-tab tutor-tab-has-seemore tutor-mt-30">
 			<?php tutor_load_template( 'single.course.enrolled.nav', array( 'course_nav_item' => $course_nav_items ) ); ?>
 			<div class="tab-body">
 				<?php
 				foreach ( $course_nav_items as $key => $subpage ) {
 					?>
-						<div class="tab-body-item <?php echo esc_attr( 'info' === $key ? 'is-active' : '' ); ?>" id="tutor-course-details-tab-<?php echo esc_attr( $key ); ?>">
+						<div class="tab-body-item <?php echo esc_attr( 'curriculum' === $key ? 'is-active' : '' ); ?>" id="tutor-course-details-tab-<?php echo esc_attr( $key ); ?>">
 						<?php
 							$method = $subpage['method'];
+
 						if ( is_string( $method ) ) {
 							$method();
 						} else {
@@ -22,6 +33,7 @@
 							$_method = $method[1];
 							$_object->$_method( get_the_ID() );
 						}
+
 						?>
 						</div>
 						<?php
@@ -29,6 +41,6 @@
 				?>
 			</div>
 		</div>
-		<?php// do_action( 'tutor_course/single/after/inner-wrap' ); ?>
-	</div>
+		<?php do_action( 'tutor_course/single/after/inner-wrap' ); ?>
 </div>
+
