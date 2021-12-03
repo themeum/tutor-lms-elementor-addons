07089e88d77df102a41ccbd9a49884add5800c48
shewa12 <shewa12kpi@gmail.com>
2021-12-03 16:34:43 +0600

    unwanted script removed

--- assets/js/tutor-elementor.js
+++ assets/js/tutor-elementor.js
@@ -1,77 +1,5 @@
 'use strict';
 (function ($) {
-	/**
-	 * Course Enroll Button
-	 * @since 1.0.0
-	 */
-	// function enroll_button($scope) {
-	// 	/**
-	// 	 * if default button 
-	// 	 * add button class
-	// 	 */
-	// 	var button_type = $scope.find("#etlms_enroll_btn_type").val();
-	// 	var cart_icon = $scope.find("#etlms_enroll_btn_cart").val();
-
-	// 	/**
-	// 	 * find tutor-loop-cart-btn-wrap from etlms-carousel-footer
-	// 	 * remove class for setting new style
-	// 	 */
-	// 	var carousel_footer = $scope.find(".etlms-carousel-footer").find('.tutor-loop-cart-btn-wrap');
-
-	// 	if (carousel_footer) {
-	// 		carousel_footer.removeClass('tutor-loop-cart-btn-wrap');
-	// 		carousel_footer.addClass('etlms-loop-cart-btn-wrap');
-	// 		/**
-	// 		 * add to cart has default button class
-	// 		 * remove it
-	// 		 * make it dynamic from elementor
-	// 		 */
-	// 		if (carousel_footer.children("a").hasClass('button')) {
-	// 			carousel_footer.children("a").removeClass('button');
-	// 		};
-	// 	}
-
-	// 	/**
-	// 	 * if default button 
-	// 	 * add button class
-	// 	 */
-	// 	if (button_type == 'default' || button_type == 'default_with_cart_icon') {
-	// 		carousel_footer.children("a").addClass('tutor-button');
-	// 	}
-
-	// 	/**
-	// 	 * if cart icon set from elementor
-	// 	 * add cart icon to all button
-	// 	 */
-	// 	if (button_type == 'text_with_cart' || button_type == 'default_with_cart_icon') {
-
-	// 		var length = carousel_footer.length;
-	// 		var i = 0;
-	// 		for (i; i < length; i++) {
-
-	// 			/*
-	// 				*check if it is add to cart
-	// 				button then add cart icon
-	// 			*/
-	// 			var is_add_to_cart = carousel_footer.children("a")[i].classList.contains('add_to_cart_button');
-	// 			var added_to_cart = carousel_footer.children("a")[i].innerHTML;
-				
-	// 			if(is_add_to_cart)
-	// 			{
-
-	// 				var text = carousel_footer.children("a")[i].innerHTML;
-	// 				carousel_footer.children("a")[i].innerHTML = `<i class="${cart_icon}" aria-hidden="true"></i> ${text}`;
-	// 			}
-				
-	// 			if(added_to_cart == "View Cart")
-	// 			{
-	// 				var text = carousel_footer.children("a")[i].innerHTML;
-	// 				carousel_footer.children("a")[i].innerHTML = `<i class="${cart_icon}" aria-hidden="true"></i> ${text}`;
-	// 			}
-
-	// 		}
-	// 	}
-	// }
 
 	jQuery(window).on('elementor/frontend/init', function () {
 		/**
@@ -170,15 +98,7 @@
 				]
 			});
 
-			//enroll_button($scope, $);  
 		});
 
-		/**
-		 * Course List
-		 * @since 1.0.0
-		 */
-		// elementorFrontend.hooks.addAction('frontend/element_ready/etlms-course-list.default', function ($scope, $) {
-		// 	//enroll_button($scope, $);
-		// });
 	});
 })(jQuery);
