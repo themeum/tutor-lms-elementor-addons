558289d47323e603e61926623c1eeaa33c344877
shewa12 <shewa12kpi@gmail.com>
2021-12-03 15:56:33 +0600

    code beautify

--- tutor-lms-elementor-addons.php
+++ tutor-lms-elementor-addons.php
@@ -12,25 +12,25 @@ License: GPLv2 or later
 Text Domain: tutor-lms-elementor-addons
 */
 
-defined('ABSPATH') || die();
+defined( 'ABSPATH' ) || die();
 
-define('ETLMS_VERSION', '1.0.4');
-define('ETLMS_FILE__', __FILE__);
-define('ETLMS_BASENAME', plugin_basename(ETLMS_FILE__));
-define('ETLMS_DIR_PATH', plugin_dir_path(ETLMS_FILE__));
-define('ETLMS_DIR_URL', plugin_dir_url(ETLMS_FILE__));
-define('ETLMS_ASSETS', trailingslashit(ETLMS_DIR_URL . 'assets'));
+define( 'ETLMS_VERSION', '1.0.4' );
+define( 'ETLMS_FILE__', __FILE__ );
+define( 'ETLMS_BASENAME', plugin_basename( ETLMS_FILE__ ) );
+define( 'ETLMS_DIR_PATH', plugin_dir_path( ETLMS_FILE__ ) );
+define( 'ETLMS_DIR_URL', plugin_dir_url( ETLMS_FILE__ ) );
+define( 'ETLMS_ASSETS', trailingslashit( ETLMS_DIR_URL . 'assets' ) );
 
 /**
  * Instantiate Base Class after plugins loaded
  */
-add_action('plugins_loaded', 'elementor_tutor_lms_init');
+add_action( 'plugins_loaded', 'elementor_tutor_lms_init' );
 function elementor_tutor_lms_init() {
-    if (!function_exists('tutor_lms') || !did_action('elementor/loaded')) {
-        require_once ETLMS_DIR_PATH . 'classes/Installer.php';
-        new \TutorLMS\Elementor\Installer();
-    } else {
-        require_once ETLMS_DIR_PATH . 'classes/Base.php';
-        \TutorLMS\Elementor\Base::instance();
-    }
+	if ( ! function_exists( 'tutor_lms' ) || ! did_action( 'elementor/loaded' ) ) {
+		require_once ETLMS_DIR_PATH . 'classes/Installer.php';
+		new \TutorLMS\Elementor\Installer();
+	} else {
+		require_once ETLMS_DIR_PATH . 'classes/Base.php';
+		\TutorLMS\Elementor\Base::instance();
+	}
 }
