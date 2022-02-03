4d593818fc53b7e59160ac6903bb6c057ed402e7
shewa12 <shewa12kpi@gmail.com>
2022-02-03 17:51:21 +0600

    update: social share controls updated

--- includes/addons/CourseSocialShare.php
+++ includes/addons/CourseSocialShare.php
@@ -173,7 +173,9 @@ class CourseSocialShare extends BaseAddon {
 	}
 
 	protected function register_style_controls() {
-		$icon_selector = '{{WRAPPER}} .etlms-social-share-wrap >button >i';
+		$share_wrapper      = '{{WRAPPER}} .etlms-course-share';
+		$popup_wrapper      = '{{WRAPPER}} #tutor-course-share-opener';
+		$popup_body_wrapper = '.etlms-course-share-popup';
 		/* Label */
 		$this->start_controls_section(
 			'course_share_label_section',
@@ -190,7 +192,7 @@ class CourseSocialShare extends BaseAddon {
 				'type'      => Controls_Manager::COLOR,
 
 				'selectors' => array(
-					'{{WRAPPER}} .etlms-social-label' => 'color: {{VALUE}};',
+					"$share_wrapper .share-text" => 'color: {{VALUE}};',
 				),
 			)
 		);
@@ -200,7 +202,7 @@ class CourseSocialShare extends BaseAddon {
 				'name'     => 'content_typography',
 				'label'    => __( 'Typography', 'tutor-lms-elementor-addons' ),
 				'scheme'   => Typography::TYPOGRAPHY_1,
-				'selector' => '{{WRAPPER}} .etlms-social-label',
+				'selector' => "$share_wrapper .share-text",
 			)
 		);
 		$this->end_controls_section();
@@ -209,183 +211,213 @@ class CourseSocialShare extends BaseAddon {
 		$this->start_controls_section(
 			'course_share_icon_section',
 			array(
-				'label' => __( 'Icon', 'tutor-lms-elementor-addons' ),
+				'label' => __( 'Share Icon', 'tutor-lms-elementor-addons' ),
 				'tab'   => Controls_Manager::TAB_STYLE,
 			)
 		);
-		$this->add_control(
-			'course_share_icon_color_settings',
-			array(
-				'label'   => __( 'Color', 'tutor-lms-elementor-addons' ),
-				'type'    => Controls_Manager::SELECT,
-				'default' => 'default',
-				'options' => array(
-					'default' => __( 'Official Color', 'tutor-lms-elementor-addons' ),
-					'custom'  => __( 'Custom', 'tutor-lms-elementor-addons' ),
-				),
-			)
-		);
-
-		$this->add_control(
-			'course_share_icon_color',
-			array(
-				'label'     => __( 'Icon Color', 'tutor-lms-elementor-addons' ),
-				'type'      => Controls_Manager::COLOR,
-				'condition' => array(
-					'course_share_icon_color_settings' => 'custom',
-				),
-				'selectors' => array(
-					'{{WRAPPER}} .etlms-share-btn >i' => 'color: {{VALUE}};',
-				),
-			)
-		);
-
-		$this->add_control(
-			'course_icon_shape_color',
-			array(
-				'label'     => __( 'Shape Color', 'tutor-lms-elementor-addons' ),
-				'type'      => Controls_Manager::COLOR,
-				'condition' => array(
-					'course_share_icon_color_settings' => 'custom',
-				),
-				'selectors' => array(
-					'{{WRAPPER}} .etlms-share-btn >i' => 'background-color: {{VALUE}};',
-				),
-			)
-		);
-
-		$this->add_responsive_control(
-			'course_share_icon_size',
-			array(
-				'label'     => __( 'Size', 'tutor-lms-elementor-addons' ),
-				'type'      => Controls_Manager::SLIDER,
-				'range'     => array(
-					'px' => array(
-						'min' => 6,
-						'max' => 100,
+			$this->add_control(
+				'course_share_icon_color_settings',
+				array(
+					'label'     => __( 'Icon Color', 'tutor-lms-elementor-addons' ),
+					'type'      => Controls_Manager::COLOR,
+					'selectors' => array(
+						"$share_wrapper a i" => 'color: {{VALUE}};',
 					),
-				),
-				'default'   => array(
-					'unit' => 'px',
-					'size' => 14,
-				),
-				'selectors' => array(
-					$icon_selector => 'font-size: {{SIZE}}{{UNIT}};',
-				),
+				)
+			);
+			$this->add_control(
+				'course_sahre_icon_size',
+				array(
+					'label'      => __( 'Icon Size', 'tutor-lms-elementor-addons' ),
+					'type'       => Controls_Manager::SLIDER,
+					'size_units' => array( 'px' ),
+					'range'      => array(
+						'px' => array(
+							'min'  => 0,
+							'max'  => 200,
+							'step' => 1,
+						),
+
+					),
+					'default'    => array(
+						'unit' => 'px',
+						'size' => 28,
+					),
+					'selectors'  => array(
+						"$share_wrapper a i" => 'width: {{SIZE}}{{UNIT}};height:{{SIZE}}{{UNIT}}',
+					),
+				)
+			);
+
+		$this->end_controls_section();
+
+		// social share icon controls.
+		$this->start_controls_section(
+			'course_social_share_icon',
+			array(
+				'label' => __( 'Social Icon', 'tutor-lms-elementor-addons' ),
+				'tab'   => Controls_Manager::TAB_STYLE,
 			)
 		);
+			$this->add_control(
+				'course_social_share_icon_color',
+				array(
+					'label'     => __( 'Icon Color', 'tutor-lms-elementor-addons' ),
+					'type'      => Controls_Manager::COLOR,
+					'condition' => array(
+						'course_share_icon_color_settings' => 'custom',
+					),
+					'selectors' => array(
+						'{{WRAPPER}} .etlms-share-btn >i' => 'color: {{VALUE}};',
+					),
+				)
+			);
 
-		$this->add_control(
-			'course_share_icon_padding',
-			array(
-				'label'      => __( 'Padding', 'tutor-lms-elementor-addons' ),
+			$this->add_control(
+				'course_social_icon_shape_color',
+				array(
+					'label'     => __( 'Shape Color', 'tutor-lms-elementor-addons' ),
+					'type'      => Controls_Manager::COLOR,
+					'condition' => array(
+						'course_share_icon_color_settings' => 'custom',
+					),
+					'selectors' => array(
+						'{{WRAPPER}} .etlms-share-btn >i' => 'background-color: {{VALUE}};',
+					),
+				)
+			);
+
+			$this->add_responsive_control(
+				'course_social_share_icon_size',
+				array(
+					'label'     => __( 'Size', 'tutor-lms-elementor-addons' ),
+					'type'      => Controls_Manager::SLIDER,
+					'range'     => array(
+						'px' => array(
+							'min' => 6,
+							'max' => 100,
+						),
+					),
+					'default'   => array(
+						'unit' => 'px',
+						'size' => 14,
+					),
+					'selectors' => array(
+						$icon_selector => 'font-size: {{SIZE}}{{UNIT}};',
+					),
+				)
+			);
+
+			$this->add_control(
+				'course_social_share_icon_padding',
+				array(
+					'label'      => __( 'Padding', 'tutor-lms-elementor-addons' ),
+					'type'       => Controls_Manager::DIMENSIONS,
+					'size_units' => array( 'px' ),
+					'selectors'  => array(
+						$icon_selector => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+					),
+					'default'    => array(
+						'top'      => 10,
+						'right'    => 10,
+						'bottom'   => 10,
+						'left'     => 10,
+						'unit'     => 'px',
+						'isLinked' => true,
+					),
+				)
+			);
+
+			$icon_spacing = is_rtl() ? 'margin-left: {{SIZE}}{{UNIT}};' : 'margin-right: {{SIZE}}{{UNIT}};';
+
+			$this->add_responsive_control(
+				'course_social_share_icon_spacing',
+				array(
+					'label'     => __( 'Spacing', 'tutor-lms-elementor-addons' ),
+					'type'      => Controls_Manager::SLIDER,
+					'range'     => array(
+						'px' => array(
+							'min' => 0,
+							'max' => 100,
+						),
+					),
+					'selectors' => array(
+						'{{WRAPPER}} .etlms-social-share-wrap >button:not(:last-child)' => $icon_spacing,
+					),
+					'default'   => array(
+						'size' => 5,
+					),
+				)
+			);
+
+			$this->add_group_control(
+				Group_Control_Border::get_type(),
+				array(
+					'name'      => 'image_border', // We know this mistake - TODO: 'icon_border' (for hover control condition also)
+					'selector'  => '{{WRAPPER}} .etlms-social-share-wrap > button >i',
+					'separator' => 'before',
+				)
+			);
+
+			$border_radius = array(
+				'label'      => __( 'Border Radius', 'tutor-lms-elementor-addons' ),
 				'type'       => Controls_Manager::DIMENSIONS,
-				'size_units' => array( 'px' ),
+				'size_units' => array( 'px', '%' ),
 				'selectors'  => array(
-					$icon_selector => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+					$icon_selector => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
 				),
 				'default'    => array(
-					'top'      => 10,
-					'right'    => 10,
-					'bottom'   => 10,
-					'left'     => 10,
+					'top'      => 0,
+					'right'    => 0,
+					'bottom'   => 0,
+					'left'     => 0,
 					'unit'     => 'px',
 					'isLinked' => true,
 				),
-			)
-		);
+			);
 
-		$icon_spacing = is_rtl() ? 'margin-left: {{SIZE}}{{UNIT}};' : 'margin-right: {{SIZE}}{{UNIT}};';
+			// for square shape
+			$border_radius['condition'] = array( 'course_share_icon_shape' => array( 'square' ) );
+			$this->add_control(
+				'course_social_share_border_radius_square',
+				$border_radius
+			);
 
-		$this->add_responsive_control(
-			'course_share_icon_spacing',
-			array(
-				'label'     => __( 'Spacing', 'tutor-lms-elementor-addons' ),
-				'type'      => Controls_Manager::SLIDER,
-				'range'     => array(
-					'px' => array(
-						'min' => 0,
-						'max' => 100,
-					),
-				),
-				'selectors' => array(
-					'{{WRAPPER}} .etlms-social-share-wrap >button:not(:last-child)' => $icon_spacing,
-				),
-				'default'   => array(
-					'size' => 5,
-				),
-			)
-		);
-
-		$this->add_group_control(
-			Group_Control_Border::get_type(),
-			array(
-				'name'      => 'image_border', // We know this mistake - TODO: 'icon_border' (for hover control condition also)
-				'selector'  => '{{WRAPPER}} .etlms-social-share-wrap > button >i',
-				'separator' => 'before',
-			)
-		);
-
-		$border_radius = array(
-			'label'      => __( 'Border Radius', 'tutor-lms-elementor-addons' ),
-			'type'       => Controls_Manager::DIMENSIONS,
-			'size_units' => array( 'px', '%' ),
-			'selectors'  => array(
-				$icon_selector => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
-			),
-			'default'    => array(
-				'top'      => 0,
-				'right'    => 0,
-				'bottom'   => 0,
-				'left'     => 0,
+			// for rounded shape
+			$border_radius['condition'] = array( 'course_share_icon_shape' => array( 'rounded' ) );
+			$border_radius['default']   = array(
+				'top'      => 5,
+				'right'    => 5,
+				'bottom'   => 5,
+				'left'     => 5,
 				'unit'     => 'px',
 				'isLinked' => true,
-			),
-		);
+			);
+			$this->add_control(
+				'course_social_share_border_radius_rounded',
+				$border_radius
+			);
 
-		// for square shape
-		$border_radius['condition'] = array( 'course_share_icon_shape' => array( 'square' ) );
-		$this->add_control(
-			'course_share_border_radius_square',
-			$border_radius
-		);
-
-		// for rounded shape
-		$border_radius['condition'] = array( 'course_share_icon_shape' => array( 'rounded' ) );
-		$border_radius['default']   = array(
-			'top'      => 5,
-			'right'    => 5,
-			'bottom'   => 5,
-			'left'     => 5,
-			'unit'     => 'px',
-			'isLinked' => true,
-		);
-		$this->add_control(
-			'course_share_border_radius_rounded',
-			$border_radius
-		);
-
-		// for rounded shape
-		$border_radius['condition'] = array( 'course_share_icon_shape' => array( 'circle' ) );
-		$border_radius['default']   = array(
-			'top'      => 50,
-			'right'    => 50,
-			'bottom'   => 50,
-			'left'     => 50,
-			'unit'     => '%',
-			'isLinked' => true,
-		);
-		$this->add_control(
-			'course_share_border_radius_circle',
-			$border_radius
-		);
-
+			// for rounded shape
+			$border_radius['condition'] = array( 'course_share_icon_shape' => array( 'circle' ) );
+			$border_radius['default']   = array(
+				'top'      => 50,
+				'right'    => 50,
+				'bottom'   => 50,
+				'left'     => 50,
+				'unit'     => '%',
+				'isLinked' => true,
+			);
+			$this->add_control(
+				'course_social_share_border_radius_circle',
+				$border_radius
+			);
 		$this->end_controls_section();
 		// hover section start
 		$this->start_controls_section(
 			'course_share_icon_hover_section',
 			array(
-				'label' => __( 'Icon Hover', 'tutor-lms-elementor-addons' ),
+				'label' => __( 'Social Icon Hover', 'tutor-lms-elementor-addons' ),
 				'tab'   => Controls_Manager::TAB_STYLE,
 			)
 		);
