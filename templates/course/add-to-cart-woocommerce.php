0b037773e02a542e16eabce378f322c00768175b
shewa12 <shewa12kpi@gmail.com>
2022-02-09 12:45:53 +0600

    new: add to cart template for woocommerce added

---
+++ templates/course/add-to-cart-woocommerce.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Woocommerce add to cart template
+ *
+ * @package ETLMSWoocommerceAddToCart
+ *
+ * @version v2.0.0
+ */
+
+$product_id = tutor_utils()->get_course_product_id();
+$product    = wc_get_product( $product_id );
+if ( $product ) {
+	if ( tutor_utils()->is_course_added_to_cart( $product_id, true ) ) {
+		?>
+			<a href="<?php echo esc_url( wc_get_cart_url() ); ?>" class="tutor-btn tutor-btn-tertiary tutor-is-outline tutor-btn-lg tutor-btn-full">
+				<?php esc_html_e( 'View Cart', 'tutor-lms-elementor-addons' ); ?>
+			</a>
+		<?php
+	} else {
+		// $sale_price    = $product->get_sale_price();
+		// $regular_price = $product->get_regular_price();
+		// $symbol        = get_woocommerce_currency_symbol();
+		?>
+		<form action="<?php echo esc_url( apply_filters( 'tutor_course_add_to_cart_form_action', get_permalink( get_the_ID() ) ) ); ?>" method="post" enctype="multipart/form-data">
+			<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>"  class="tutor-btn tutor-btn-icon tutor-btn-primary tutor-btn-lg tutor-btn-full tutor-mt-24 tutor-add-to-cart-button">
+				<span class="btn-icon tutor-icon-cart-filled"></span>
+				<span><?php echo esc_html( $product->single_add_to_cart_text() ); ?></span>
+			</button>
+		</form>
+		<?php
+	}
+} else {
+	?>
+	<p class="tutor-alert-warning">
+		<?php esc_html_e( 'Please make sure that your product exists and valid for this course', 'tutor-lms-elementor-addons' ); ?>
+	</p>
+	<?php
+}
