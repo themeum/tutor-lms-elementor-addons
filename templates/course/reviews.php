2a5d68e21498ef684b8bab8239536eae2799a259
shewa12 <shewa12kpi@gmail.com>
2022-03-29 13:56:52 +0600

    fix: wp coding standard error fixed

--- templates/course/reviews.php
+++ templates/course/reviews.php
@@ -41,12 +41,12 @@ do_action( 'tutor_course/single/enrolled/before/reviews' );
 	<h3 class="tutor-fs-5 tutor-fw-bold tutor-color-black tutor-mb-24">
 		<?php
 			$review_title = apply_filters( 'tutor_course_reviews_section_title', 'Student Ratings & Reviews' );
-			echo esc_html( $review_title, 'tutor' );
+			echo esc_html( $review_title, 'tutor-lms-elementor-addons' );
 		?>
 	</h3>
 
 	<?php if ( ! is_array( $reviews ) || ! count( $reviews ) ) : ?>
-		<?php tutor_utils()->tutor_empty_state( __( 'No Review Yet', 'tutor' ) ); ?>
+		<?php tutor_utils()->tutor_empty_state( __( 'No Review Yet', 'tutor-lms-elementor-addons' ) ); ?>
 	<?php else : ?>
 		<div class="tutor-ratingsreviews">
 			<div class="tutor-ratingsreviews-ratings">
@@ -57,13 +57,13 @@ do_action( 'tutor_course/single/enrolled/before/reviews' );
 					<?php tutor_utils()->star_rating_generator_v2( $rating->rating_avg, null, false, 'tutor-d-block', 'lg' ); ?>
 					<div class="tutor-total-ratings-text tutor-fs-6 tutor-black-60 tutor-mt-12">
 						<span class="tutor-rating-text-part">
-							<?php esc_html_e( 'Total ', 'tutor' ); ?>
+							<?php esc_html_e( 'Total ', 'tutor-lms-elementor-addons' ); ?>
 						</span>
 						<span class="tutor-rating-count-part">
 							<?php echo esc_html( count( $reviews ) ); ?>
 						</span>
 						<span class="tutor-rating-text-part">
-							<?php echo esc_html( _n( ' Rating', ' Ratings', count( $reviews ), 'tutor' ) ); ?>
+							<?php echo esc_html( _n( ' Rating', ' Ratings', count( $reviews ), 'tutor-lms-elementor-addons' ) ); ?>
 						</span>
 					</div>
 				</div>
@@ -88,7 +88,7 @@ do_action( 'tutor_course/single/enrolled/before/reviews' );
 							<div class="rating-num tutor-fs-7 tutor-color-black-60">
 								<?php
 									echo $value . ' ';
-									echo $value > 1 ? __( 'ratings', 'tutor' ) : __( 'rating', 'tutor' );
+									echo $value > 1 ? __( 'ratings', 'tutor-lms-elementor-addons' ) : __( 'rating', 'tutor-lms-elementor-addons' );
 								?>
 							</div>
 						</div>
@@ -111,7 +111,7 @@ do_action( 'tutor_course/single/enrolled/before/reviews' );
 					<i class="tutor-icon-star-line-filled tutor-icon-24 tutor-mr-4"></i>
 					<?php
 						$is_new = ! $my_rating || empty( $my_rating->rating ) || empty( $my_rating->comment_content );
-						$is_new ? _e( 'Write a review', 'tutor' ) : _e( 'Edit review', 'tutor' );
+						$is_new ? esc_html_e( 'Write a review', 'tutor-lms-elementor-addons' ) : esc_html_e( 'Edit review', 'tutor-lms-elementor-addons' );
 					?>
 				</button>
 			<?php endif; ?>
@@ -124,7 +124,7 @@ do_action( 'tutor_course/single/enrolled/before/reviews' );
 					'paged'       => $current_page,
 					'layout'      => array(
 						'type'           => 'load_more',
-						'load_more_text' => __( 'Load More', 'tutor' ),
+						'load_more_text' => __( 'Load More', 'tutor-lms-elementor-addons' ),
 					),
 					'ajax'        => array(
 						'action'    => 'tutor_single_course_reviews_load_more',
@@ -144,8 +144,8 @@ do_action( 'tutor_course/single/enrolled/before/reviews' );
 		<div class="tutor-write-review-form" style="display: none;">
 			<form method="post">
 				<div class="tutor-star-rating-container">
-					<input type="hidden" name="course_id" value="<?php echo $course_id; ?>"/>
-					<input type="hidden" name="review_id" value="<?php echo $my_rating ? $my_rating->comment_ID : ''; ?>"/>
+					<input type="hidden" name="course_id" value="<?php echo esc_attr( $course_id ); ?>"/>
+					<input type="hidden" name="review_id" value="<?php echo esc_attr( $my_rating ? $my_rating->comment_ID : '' ); ?>"/>
 					<input type="hidden" name="action" value="tutor_place_rating"/>
 					<div class="tutor-form-group">
 						<?php
@@ -153,11 +153,11 @@ do_action( 'tutor_course/single/enrolled/before/reviews' );
 						?>
 					</div>
 					<div class="tutor-form-group">
-						<textarea name="review" placeholder="<?php _e( 'write a review', 'tutor' ); ?>"><?php echo stripslashes( $my_rating ? $my_rating->comment_content : '' ); ?></textarea>
+						<textarea name="review" placeholder="<?php esc_html_e( 'write a review', 'tutor-lms-elementor-addons' ); ?>"><?php echo esc_textarea( $my_rating ? $my_rating->comment_content : '' ); ?></textarea>
 					</div>
 					<div class="tutor-form-group">
 						<button type="submit" class="tutor_submit_review_btn tutor-btn tutor-btn-primary">
-							<?php _e( 'Submit Review', 'tutor' ); ?>
+							<?php esc_html_e( 'Submit Review', 'tutor-lms-elementor-addons' ); ?>
 						</button>
 					</div>
 				</div>
