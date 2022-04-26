7c8c5c573e674a2045d039daed01aaa9ca47bfb5
shewa12 <shewa12kpi@gmail.com>
2022-04-26 10:59:09 +0600

    update: course content template updated

--- templates/course/content.php
+++ templates/course/content.php
@@ -6,59 +6,41 @@
  */
 
 $course_nav_items = apply_filters( 'tutor_course/single/nav_items', tutor_utils()->course_nav_items(), get_the_ID() );
-?>
-<?php do_action('tutor_course/single/before/wrap'); ?>
-<div <?php tutor_post_class('tutor-full-width-course-top tutor-course-top-info tutor-page-wrap etlms-course-content'); ?>>
-    <div class="tutor-course-details-page tutor-container">
-        <?php (isset($is_enrolled) && $is_enrolled) ? tutor_course_enrolled_lead_info() : tutor_course_lead_info(); ?>
-        <div class="tutor-row tutor-gx-xl-5">
-            <main class="tutor-col-xl-8">
-                <?php tutor_utils()->has_video_in_single() ? tutor_course_video() : get_tutor_course_thumbnail(); ?>
-	            <?php do_action('tutor_course/single/before/inner-wrap'); ?>
-                <div class="tutor-course-details-tab tutor-tab-has-seemore tutor-mt-32">
-                    <div class="tutor-is-sticky">
-                        <?php tutor_load_template( 'single.course.enrolled.nav', array('course_nav_item' => $course_nav_item ) ); ?>
-                    </div>
-                    <div class="tutor-tab tutor-pt-24">
-                        <?php foreach( $course_nav_item as $key => $subpage ) : ?>
-                            <div id="tutor-course-details-tab-<?php echo $key; ?>" class="tutor-tab-item<?php echo $key == 'info' ? ' is-active' : ''; ?>">
-                                <?php
-                                    do_action( 'tutor_course/single/tab/'.$key.'/before' );
-                                    $method = $subpage['method'];
-									if ( 'info' === $key ) {
-										include ETLMS_TEMPLATE . 'about.php';
-										include ETLMS_TEMPLATE . 'benefits.php';
-										include ETLMS_TEMPLATE . 'instructors.php';
-									} else {
-										if ( is_string( $method ) ) {
-											$method();
-										} else {
-											$_object = $method[0];
-											$_method = $method[1];
-											$_object->$_method( get_the_ID() );
-										}
-									}
-                                    do_action( 'tutor_course/single/tab/'.$key.'/after' );
-                                ?>
-                            </div>
-                        <?php endforeach; ?>
-                    </div>
-                </div>
-	            <?php do_action('tutor_course/single/after/inner-wrap'); ?>
-            </main>
 
-            <aside class="tutor-col-xl-4">
-                <div class="tutor-single-course-sidebar tutor-mt-40 tutor-mt-xl-0">
-                    <?php do_action('tutor_course/single/before/sidebar'); ?>
-                    <?php tutor_load_template('single.course.course-entry-box'); ?>
-                    <?php tutor_course_requirements_html(); ?>
-                    <?php tutor_course_tags_html(); ?>
-                    <?php tutor_course_target_audience_html(); ?>
-                    <?php do_action('tutor_course/single/after/sidebar'); ?>
-                </div>
-            </aside>
-        </div>
-    </div>
+?>
+<div class="tutor-wrap etlms-course-curriculum">
+	<?php do_action( 'tutor_course/single/before/inner-wrap' ); ?>
+	<div class="tutor-course-details-tab tutor-tab-has-seemore tutor-mt-32">
+		<div class="tutor-is-sticky">
+			<?php tutor_load_template( 'single.course.enrolled.nav', array( 'course_nav_item' => $course_nav_items ) ); ?>
+		</div>
+		<div class="tutor-tab tutor-pt-24">
+			<?php
+			foreach ( $course_nav_items as $key => $subpage ) {
+				?>
+					<div id="tutor-course-details-tab-<?php echo $key; ?>" class="tutor-tab-item<?php echo $key == 'info' ? ' is-active' : ''; ?>">
+					<?php
+						$method = $subpage['method'];
+					if ( 'info' === $key ) {
+						include ETLMS_TEMPLATE . 'about.php';
+						include ETLMS_TEMPLATE . 'benefits.php';
+						include ETLMS_TEMPLATE . 'instructors.php';
+					} else {
+						if ( is_string( $method ) ) {
+							$method();
+						} else {
+							$_object = $method[0];
+							$_method = $method[1];
+							$_object->$_method( get_the_ID() );
+						}
+					}
+					?>
+					</div>
+					<?php
+			}
+			?>
+		</div>
+	</div>
+	<?php do_action( 'tutor_course/single/after/inner-wrap' ); ?>
 </div>
-<?php do_action('tutor_course/single/after/wrap'); ?>
 
