89b6423d673940384d3073e6361bf2393254b858
shewa12 <shewa12kpi@gmail.com>
2022-04-27 15:35:41 +0600

    fix: undefined variable  fixed

--- templates/course/list/parts/thumbnail.php
+++ templates/course/list/parts/thumbnail.php
@@ -6,7 +6,7 @@
     <div class="tutor-course-thumbnail">
         <a href="<?php the_permalink(); ?>" class="tutor-d-block">
             <div class="tutor-ratio tutor-ratio-<?php echo $settings['course_list_skin'] == 'overlayed' ? '1x1' : '16x9'; ?>">
-                <img class="tutor-card-image-top" src="<?php echo $image_url; ?>" alt="<?php the_title(); ?>" style="<?php echo esc_attr( $dynamic_style ); ?>" loading="lazy">
+                <img class="tutor-card-image-top" src="<?php echo $image_url; ?>" alt="<?php the_title(); ?>" style="<?php echo esc_attr( isset( $dynamic_style ) ? $dynamic_style : '' ); ?>" loading="lazy">
             </div>
         </a>
     </div>
