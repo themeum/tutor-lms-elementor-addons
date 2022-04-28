5030e4149764fda26c5bc0c7a43cdef2b1ade44a
shewa12 <shewa12kpi@gmail.com>
2022-04-28 12:31:54 +0600

    fix: undefined index error fixed

--- templates/course/carousel/parts/thumbnail.php
+++ templates/course/carousel/parts/thumbnail.php
@@ -1,12 +1,12 @@
 <?php if ( 'yes' == $settings['course_carousel_image'] ) : ?>
     <?php
-        $image_size = $settings['course_carousel_image_size'];
+        $image_size = isset( $settings['course_carousel_image_size'] ) ? $settings['course_carousel_image_size'] : 'medium_large';
         $image_url  = get_tutor_course_thumbnail( $image_size, $url = true );
     ?>
     <div class="tutor-course-thumbnail">
         <a href="<?php the_permalink(); ?>" class="tutor-d-block">
-            <div class="tutor-ratio tutor-ratio-<?php echo $settings['course_list_skin'] == 'overlayed' ? '1x1' : '16x9'; ?>">
-                <img class="tutor-card-image-top" src="<?php echo $image_url; ?>" alt="<?php the_title(); ?>" loading="lazy">
+            <div class="tutor-ratio tutor-ratio-<?php echo esc_attr( isset( $settings['course_list_skin'] ) && 'overlayed' === $settings['course_list_skin'] ? '1x1' : '16x9' ); ?>">
+                <img class="tutor-card-image-top" src="<?php echo esc_url( $image_url ); ?>" alt="<?php the_title(); ?>" loading="lazy">
             </div>
         </a>
     </div>
