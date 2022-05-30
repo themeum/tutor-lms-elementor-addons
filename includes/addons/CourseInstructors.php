a80ab1c442f8f7e5d86ea0be7b0bb5463a530608
shewa12 <shewa12kpi@gmail.com>
2022-05-30 13:37:23 +0600

    update: course instructor addon updated

--- includes/addons/CourseInstructors.php
+++ includes/addons/CourseInstructors.php
@@ -71,9 +71,9 @@ class CourseInstructors extends BaseAddon {
 		$this->add_control(
 			'about_the_instructors_title',
 			array(
-				'label'       => __( 'About the Instructors Title', 'tutor-lms-elementor-addons' ),
+				'label'       => __( 'Title', 'tutor-lms-elementor-addons' ),
 				'type'        => Controls_Manager::TEXTAREA,
-				'default'     => __( 'About the instructors', 'tutor-lms-elementor-addons' ),
+				'default'     => __( 'A course by', 'tutor-lms-elementor-addons' ),
 				'placeholder' => __( 'Type your title here', 'tutor-lms-elementor-addons' ),
 				'rows'        => 3,
 			)
@@ -116,18 +116,6 @@ class CourseInstructors extends BaseAddon {
 			)
 		);
 
-		$this->add_control(
-			'course_instructor_bio',
-			array(
-				'label'        => __( 'Bio', 'tutor-lms-elementor-addons' ),
-				'type'         => Controls_Manager::SWITCHER,
-				'label_on'     => __( 'Show', 'tutor-lms-elementor-addons' ),
-				'label_off'    => __( 'Hide', 'tutor-lms-elementor-addons' ),
-				'return_value' => 'yes',
-				'default'      => 'yes',
-			)
-		);
-
 		// link.
 		$this->add_control(
 			'course_instructor_link',
@@ -136,12 +124,11 @@ class CourseInstructors extends BaseAddon {
 				'type'        => Controls_Manager::SELECT,
 				'description' => __( 'Link for the Author Name and Image', 'tutor-lms-elementor-addons' ),
 				'options'     => array(
-					'none'        => 'None',
-					'new_window'  => 'New Window',
-					'same_window' => 'Same Window',
+					'_blank'  => 'New Window',
+					'' => 'Same Window',
 				),
 
-				'default'     => 'new_window',
+				'default'     => '_blank',
 			)
 		);
 
@@ -160,7 +147,7 @@ class CourseInstructors extends BaseAddon {
 		$this->start_controls_section(
 			'course_instructors_title_section',
 			array(
-				'label' => __( 'Course Instructor Title', 'tutor-lms-elementor-addons' ),
+				'label' => __( 'Title', 'tutor-lms-elementor-addons' ),
 				'tab'   => Controls_Manager::TAB_STYLE,
 			)
 		);
@@ -171,7 +158,7 @@ class CourseInstructors extends BaseAddon {
 				'label'     => __( 'Color', 'tutor-lms-elementor-addons' ),
 				'type'      => Controls_Manager::COLOR,
 				'selectors' => array(
-					"$selector .etlms-course-instructor-title" => 'color: {{VALUE}};',
+					"$selector h3" => 'color: {{VALUE}};',
 				),
 			)
 		);
@@ -181,7 +168,7 @@ class CourseInstructors extends BaseAddon {
 			array(
 				'name'     => 'course_instructors_title_typo',
 				'label'    => __( 'Typography', 'tutor-lms-elementor-addons' ),
-				'selector' => "$selector .etlms-course-instructor-title",
+				'selector' => "$selector h3",
 			)
 		);
 
@@ -198,7 +185,7 @@ class CourseInstructors extends BaseAddon {
 					),
 				),
 				'selectors'  => array(
-					"$selector .etlms-course-instructor-title" => 'margin-bottom: {{SIZE}}{{UNIT}} !important;',
+					"$selector h3" => 'margin-bottom: {{SIZE}}{{UNIT}} !important;',
 				)
 			)
 		);
@@ -243,14 +230,14 @@ class CourseInstructors extends BaseAddon {
 				'separator'  => 'after',
 			)
 		);
-
+	
 		$this->add_control(
 			'course_instructor_name_color',
 			array(
 				'label'     => __( 'Name Color', 'tutor-lms-elementor-addons' ),
 				'type'      => Controls_Manager::COLOR,
 				'selectors' => array(
-					$selector . ' .etlms-instructor-name', $selector . ' .etlms-instructor-name a' => 'color: {{VALUE}}',
+					$selector . ' .tutor-instructor-name' => 'color: {{VALUE}}',
 				)
 			)
 		);
@@ -260,7 +247,7 @@ class CourseInstructors extends BaseAddon {
 			array(
 				'name'     => 'course_instructor_name_typo',
 				'label'    => __( 'Name Typography', 'tutor-lms-elementor-addons' ),
-				'selector' => $selector . ' .etlms-instructor-name',
+				'selector' => $selector . ' .tutor-instructor-name'
 			)
 		);
 
@@ -270,7 +257,7 @@ class CourseInstructors extends BaseAddon {
 				'label'     => __( 'Designation Color', 'tutor-lms-elementor-addons' ),
 				'type'      => Controls_Manager::COLOR,
 				'selectors' => array(
-					$selector . ' .etlms-instructor-designation' => 'color: {{VALUE}}',
+					$selector . ' .tutor-instructor-designation' => 'color: {{VALUE}};',
 				)
 			)
 		);
@@ -280,165 +267,7 @@ class CourseInstructors extends BaseAddon {
 			array(
 				'name'     => 'course_instructor_designation_typo',
 				'label'    => __( 'Designation Typography', 'tutor-lms-elementor-addons' ),
-				'selector' => $selector . ' .etlms-instructor-designation',
-			)
-		);
-
-		$this->add_control(
-			'course_instructor_bio_color',
-			array(
-				'label'     => __( 'Biography Color', 'tutor-lms-elementor-addons' ),
-				'type'      => Controls_Manager::COLOR,
-				'selectors' => array(
-					$selector . ' .etlms-instructor-bio' => 'color: {{VALUE}} !important;',
-				)
-			)
-		);
-
-		$this->add_group_control(
-			Group_Control_Typography::get_type(),
-			array(
-				'name'     => 'course_instructor_bio_typo',
-				'label'    => __( 'Biography Typography', 'tutor-lms-elementor-addons' ),
-				'selector' => $selector . ' .etlms-instructor-bio',
-			)
-		);
-
-		$this->end_controls_section();
-
-		/* Bottom Info Section */
-		$this->start_controls_section(
-			'course_instructor_bottom_info_section',
-			array(
-				'label' => __( 'Instructor Bottom Info', 'tutor-lms-elementor-addons' ),
-				'tab'   => Controls_Manager::TAB_STYLE,
-			)
-		);
-
-		$this->add_control(
-			'course_instructor_rating_color',
-			array(
-				'label'     => __( 'Rating Color', 'tutor-lms-elementor-addons' ),
-				'type'      => Controls_Manager::COLOR,
-				'selectors' => array(
-					$selector . ' .tutor-ratings-stars' => 'color: {{VALUE}};'
-				)
-			)
-		);
-
-		$this->add_control(
-			'course_instructor_rating_size',
-			array(
-				'label'      => __( 'Rating Size', 'tutor-lms-elementor-addons' ),
-				'type'       => Controls_Manager::SLIDER,
-				'size_units' => array( 'px' ),
-				'range'      => array(
-					'px' => array(
-						'min' => 5,
-						'max' => 64,
-					),
-				),
-				'selectors'  => array(
-					$selector . ' .tutor-ratings-stars' => 'font-size: {{SIZE}}{{UNIT}};'
-				)
-			)
-		);
-
-		$this->add_control(
-			'course_instructor_label_color',
-			array(
-				'label'     => __( 'Label Color', 'tutor-lms-elementor-addons' ),
-				'type'      => Controls_Manager::COLOR,
-				'selectors' => array(
-					$selector . ' .tutor-ratings-average' => 'color: {{VALUE}};',
-					$selector . ' .tutor-meta-value' => 'color: {{VALUE}};'
-				),
-			)
-		);
-
-		$this->add_group_control(
-			Group_Control_Typography::get_type(),
-			array(
-				'name'     => 'course_instructor_label_typography',
-				'label'    => __( 'Label Typography', 'tutor-lms-elementor-addons' ),
-				'selector' => "$selector .tutor-ratings-average, $selector .tutor-meta-value",
-				'scheme'   => Typography::TYPOGRAPHY_1,
-			)
-		);
-
-		$this->add_control(
-			'course_instructor_value_color',
-			array(
-				'label'     => __( 'Value Color', 'tutor-lms-elementor-addons' ),
-				'type'      => Controls_Manager::COLOR,
-				'selectors' => array(
-					$selector . ' .tutor-ratings-count' => 'color: {{VALUE}} !important;',
-					$selector . ' .tutor-meta-key' => 'color: {{VALUE}} !important;'
-				)
-			)
-		);
-
-		$this->add_group_control(
-			Group_Control_Typography::get_type(),
-			array(
-				'name'     => 'course_instructor_value_typography',
-				'label'    => __( 'Value Typography', 'tutor-lms-elementor-addons' ),
-				'selector' => "$selector .tutor-ratings-average, $selector .tutor-meta-value",
-			)
-		);
-
-		$this->add_control(
-			'course_instructor_bottom_info_icon_size',
-			array(
-				'label'      => __( 'Icon Size', 'tutor-lms-elementor-addons' ),
-				'type'       => Controls_Manager::SLIDER,
-				'size_units' => array( 'px' ),
-				'range'      => array(
-					'px' => array(
-						'min' => 5,
-						'max' => 64,
-					),
-				),
-				'selectors'  => array(
-					$selector . ' .etlms-meta-icon' => 'font-size: {{SIZE}}{{UNIT}};',
-				)
-			)
-		);
-
-		$this->end_controls_section();
-
-		// spacing section.
-		$this->start_controls_section(
-			'course_instructors_space_section',
-			array(
-				'label' => __( 'Instructor Spacing', 'tutor-lms-elementor-addons' ),
-				'tab'   => Controls_Manager::TAB_STYLE,
-			)
-		);
-
-		$this->add_control(
-			'course_instructors_padding',
-			array(
-				'label'      => __( 'Instructor Padding', 'tutor-lms-elementor-addons' ),
-				'type'       => Controls_Manager::DIMENSIONS,
-				'size_units' => array( 'px', '%', 'em' ),
-				'selectors'  => array(
-					$selector . ' .tutor-card-body' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}} !important;',
-				),
-				'separator'  => 'before',
-			)
-		);
-
-		$this->add_control(
-			'course_instructors_bottom_padding',
-			array(
-				'label'      => __( 'Bottom Info Padding', 'tutor-lms-elementor-addons' ),
-				'type'       => Controls_Manager::DIMENSIONS,
-				'size_units' => array( 'px', '%', 'em' ),
-				'selectors'  => array(
-					"$selector .tutor-card-footer" => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
-				),
-				'separator'  => 'before',
+				'selector' => $selector . ' .tutor-instructor-designation'
 			)
 		);
 
@@ -453,17 +282,14 @@ class CourseInstructors extends BaseAddon {
 	 * @return void
 	 */
 	protected function render( $instance = array() ) {
-		global $wp_query;
-		if ( empty( $wp_query->query_vars['course_subpage'] ) ) {
-			$course = etlms_get_course();
-			if ( $course ) {
-				ob_start();
-				$settings = $this->get_settings_for_display();
-				include etlms_get_template( 'course/instructors' );
-				$output = apply_filters( 'tutor_course/single/instructors_html', ob_get_clean() );
-				// PHPCS - the variable $output holds safe data.
-				echo $output; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-			}
+		$course = etlms_get_course();
+		if ( $course ) {
+			ob_start();
+			$settings = $this->get_settings_for_display();
+			include etlms_get_template( 'course/instructors' );
+			$output = apply_filters( 'tutor_course/single/instructors_html', ob_get_clean() );
+			// PHPCS - the variable $output holds safe data.
+			echo $output; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 		}
 	}
 }
--- templates/course/instructors.php
+++ templates/course/instructors.php
@@ -9,7 +9,8 @@
  * @package TutorLMS/Templates
  */
 
-// Settings from elementor
+// Settings from Elementor.
+$title 					= $settings['about_the_instructors_title'];
 $show_profile_picture 	= $settings['course_instructor_profile'];
 $display_name         	= $settings['course_instructor_name'];
 $designation          	= $settings['course_instructor_designation'];
@@ -17,84 +18,44 @@ $profile_link         	= $settings['course_instructor_link'];
 $target_blank         	= ( $settings['course_instructor_link'] == 'new_window' ) ? 'target="_blank"' : '';
 $border_radius        	= $settings['course_instructors_avatar_border_radius'];
 $instructors 			= tutor_utils()->get_instructors_by_course();
+
+do_action( 'tutor_course/single/enrolled/before/instructors' );
+
+$instructors = tutor_utils()->get_instructors_by_course();
+$is_enabled  = tutor_utils()->get_option( 'display_course_instructors' );
+if ( ! $is_enabled ) {
+	return esc_html_e( 'Please enable Instructor Info from Tutor settings ', 'tutor-lms-elementor-addons' );
+}
 ?>
 
-<?php do_action( 'tutor_course/single/enrolled/before/instructors' ); ?>
+<?php if ( $instructors && count( $instructors ) ) : ?>
+<div class="tutor-course-details-instructors etlms-course-instructors">
+	<h3 class="tutor-fs-6 tutor-fw-medium tutor-color-black tutor-mb-16">
+		<?php echo esc_html( $title ); ?>
+	</h3>
 
-<?php if($instructors && count($instructors)) : ?>
-	<div class="etlms-course-instructors">
-		<h3 class="etlms-course-instructor-title tutor-fs-5 tutor-fw-bold tutor-color-black tutor-mb-24">
-			<?php echo esc_html( $settings['about_the_instructors_title'], 'tutor-lms-elementor-addons' ); ?>
-		</h3>
-
-		<?php foreach($instructors as $instructor): ?>
-			<div class="tutor-card etlms-card tutor-mb-32">
-				<div class="tutor-card-body">
-					<div class="tutor-d-flex">
-						<?php if ( $show_profile_picture == 'yes' ) : ?>
-							<div class="tutor-mr-16">
-								<?php echo tutor_utils()->get_tutor_avatar($instructor->ID, 'md'); ?>
-							</div>
-						<?php endif; ?>
-
-						<div>
-							<?php if ( $display_name == 'yes' ) : ?>
-							<div class="etlms-instructor-name tutor-fs-6 tutor-fw-medium">
-								<a class="tutor-color-black" href="<?php echo tutor_utils()->profile_url($instructor->ID, true); ?>"><?php echo $instructor->display_name; ?></a>
-							</div>
-							<?php endif; ?>
-							
-							<?php if ( 'yes' === $designation && ! empty( $instructor->tutor_profile_job_title ) ) : ?>
-								<div class="etlms-instructor-designation tutor-fs-7 tutor-color-muted tutor-break-word tutor-mt-4">
-									<?php echo esc_html( $instructor->tutor_profile_job_title ); ?>
-								</div>
-							<?php endif; ?>
-
-							<?php if ( 'yes' === $settings['course_instructor_bio'] ) : ?>
-								<div class="etlms-instructor-bio tutor-fs-6 tutor-color-secondary tutor-mt-20">
-									<?php echo tutor_utils()->clean_html_content( $instructor->tutor_profile_bio ); ?>
-								</div>
-							<?php endif; ?>
-						</div>
-					</div>
+	<?php foreach ( $instructors as $key => $instructor ) : ?>
+		<div class="tutor-d-flex tutor-align-center<?php echo ( $key != count( $instructors ) - 1 ) ? ' tutor-mb-24' : ''; ?>">
+			<?php if ( 'yes' === $show_profile_picture ) : ?>
+				<div class="tutor-d-flex tutor-mr-16">
+					<?php echo tutor_utils()->get_tutor_avatar( $instructor->ID, 'md' ); ?>
 				</div>
-
-				<div class="tutor-card-footer">
-					<div class="tutor-row">
-						<div class="tutor-col tutor-mb-12 tutor-mb-md-0">
-							<?php 
-								$instructor_rating = tutor_utils()->get_instructor_ratings( $instructor->ID );
-								tutor_utils()->star_rating_generator_v2( $instructor_rating->rating_avg, $instructor_rating->rating_count, true ); 
-							?>
-						</div>
-						<div class="tutor-col-md-auto">
-							<div class="tutor-meta">
-								<div>
-									<span class="etlms-meta-icon tutor-icon-user-line tutor-meta-icon tutor-color-black" area-hidden="true"></span>
-									<span class="tutor-meta-value">
-										<?php echo tutor_utils()->get_total_students_by_instructor($instructor->ID); ?>
-									</span>
-									<span class="tutor-meta-key">
-										<?php _e('Students', 'tutor'); ?>
-									</span>
-								</div>
-								
-								<div>
-									<span class="etlms-meta-icon tutor-icon-mortarboard tutor-meta-icon tutor-color-black" area-hidden="true"></span>
-									<span class="tutor-meta-value">
-										<?php echo tutor_utils()->get_course_count_by_instructor($instructor->ID); ?>
-									</span>
-									<span class="tutor-meta-key">
-										<?php _e('Courses', 'tutor'); ?>
-									</span>
-								</div>
-							</div>
-						</div>
+			<?php endif; ?>
+			<div>
+				<?php if ( 'yes' === $display_name ) : ?>
+					<a class="tutor-instructor-name tutor-fs-6 tutor-fw-bold tutor-color-black" href="<?php echo tutor_utils()->profile_url( $instructor->ID, true ); ?>" target="<?php echo esc_attr( $profile_link ); ?>">
+						<?php echo esc_html( $instructor->display_name ); ?>
+					</a>
+				<?php endif; ?>
+				<?php if ( 'yes' === $designation && ! empty( $instructor->tutor_profile_job_title ) ) : ?>
+					<div class="tutor-instructor-designation tutor-fs-7 tutor-color-muted">
+						<?php echo $instructor->tutor_profile_job_title; ?>
 					</div>
-				</div>
+				<?php endif; ?>
 			</div>
-		<?php endforeach; ?>
-	</div>
-<?php endif; ?>
+		</div>
+	<?php endforeach; ?>
+</div>
+<?php endif;
 
-<?php do_action( 'tutor_course/single/enrolled/after/instructors' ); ?>
+do_action( 'tutor_course/single/enrolled/after/instructors' );
