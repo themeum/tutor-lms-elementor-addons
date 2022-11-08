2e6115d6168d197b47c9eb37c28567f86387c682
Md Rashed Hossain <wprashed@icloud.com>
2022-11-08 22:26:00 +0600

    Update tutor-lms-elementor-addons.php

--- tutor-lms-elementor-addons.php
+++ tutor-lms-elementor-addons.php
@@ -4,7 +4,7 @@ Plugin Name: Tutor LMS Elementor Addons
 Plugin URI: https://www.themeum.com/product/tutor-lms/
 Description: Elementor Addons Integration - Tutor LMS plugin lets you design course pages with Elementor.
 Author: Themeum
-Version: 2.0.5
+Version: 2.0.6
 Author URI: http://themeum.com
 Requires at least: 5.3
 Tested up to: 6.0.2
