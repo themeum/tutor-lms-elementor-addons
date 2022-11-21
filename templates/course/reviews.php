d7cf147c69ae637fab13fb33d16c7f3354eaf5e7
Md Rashed Hossain <wprashed@icloud.com>
2022-11-21 13:49:03 +0600

    Revert "Update reviews.php"
    This reverts commit cb8c25d3a5a4acaf7c7b1a5949aecddcde0a31e9.

--- templates/course/reviews.php
+++ templates/course/reviews.php
@@ -112,6 +112,7 @@ do_action( 'tutor_course/single/enrolled/before/reviews' );
 		</div>
 	<?php endif; ?>
 
+	
 	<div class="tutor-row tutor-mt-40 tutor-mb-20">
 		<div class="tutor-col">
 			<?php if ( $is_enrolled ) : ?>
