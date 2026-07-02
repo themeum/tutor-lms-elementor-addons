f7e7897863046c074f7d3d052a562d9bbbac080b
Anindra Das Bivas <bivasdas911@gmail.com>
2026-07-02 13:05:37 +0600

    update block comment

--- tutor-lms-elementor-addons.php
+++ tutor-lms-elementor-addons.php
@@ -7,7 +7,7 @@
  * Version: 4.0.0
  * Author URI: http://themeum.com
  * Requires at least: 5.3
- * Tested up to: 6.9.1
+ * Tested up to: 7.0.0
  * License: GPLv2 or later
  * Text Domain: tutor-lms-elementor-addons
  *
